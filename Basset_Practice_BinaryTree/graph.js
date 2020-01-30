cbe87ff4b36ce2c8f20c8e874cff195c307253da
Yurie <cutecupcakeandcute@gmail.com>
2020-01-30 17:28:38 +0900

    create recursion

--- Basset_Practice_BinaryTree/graph.js
+++ Basset_Practice_BinaryTree/graph.js
@@ -1,29 +1,62 @@
-const transactions = [
-    { source: 0, targets: [1, 2], amounts: [1.5, 0.5] },
-    { source: 1, targets: [3, 4], amounts: [1.2, 0.3] },
-    { source: 3, targets: [5, 6], amounts: [0.8, 0.2] }
-]
-
-
 function treeConverter(arr) {
     let filtered = [];
     for (let i = 0; i < arr.length; i++) {
-        // console.log(arr[i])
         let treeData = {
             name: "",
             children: []
         };
         treeData.name = arr[i].source
         for (let j = 0; j < arr[i].targets.length; j++) {
-            // console.log(`This is target ${arr[i].targets[j]}`)
             let obj = {};
             obj["name"] = arr[i].targets[j]
-            // console.log(arr[i].targets[j])
             treeData.children.push(obj)
         }
         filtered.push(treeData)
     }
-    console.log(filtered)
 }
 
-console.log(treeConverter(transactions))
+const transactions = [
+    { source: 0, targets: [1, 2], amounts: [1.5, 0.5] },
+    { source: 1, targets: [3, 4], amounts: [1.2, 0.3] },
+    { source: 3, targets: [5, 6], amounts: [0.8, 0.2] }
+]
+
+const dataFormat = {
+    name: "",
+    children: []
+}
+
+function recursion (obj) {
+    dataFormat.name = obj.source
+    for (let i = 0; i < obj.targets.length; i++) {
+        let childNode = {}
+        childNode["name"] = obj.targets[i]
+        dataFormat.children.push(childNode)
+    }
+    console.log(obj)
+    // for (let k = 0; k < obj.targets.length; k++) {
+    //     console.log(`index num of obj targets: ${k}`)
+    //     console.log(`obj.targets: ${obj.targets[k]}`)
+    //     for (let j = 0; j < transactions.length; j++) {
+    //         // console.log(`index number of transactions: ${j}`)
+    //         // console.log(transactions[j])
+    //         console.log('dataFormat')
+    //         console.log(dataFormat.children[k].name)
+    //         // console.log(transactions[j])
+    //         // if (dataFormat.children[k].name === transactions[j].source) {
+    //         //     console.log(dataFormat.children[k].name)
+    //         //     console.log(transactions[j])
+    //         //     recursion(transactions[j])
+    //         // }
+    //     }
+    // }
+
+    // recursion(dataFormat.children[0])
+    console.log(dataFormat)
+}
+
+function main (arr) {
+    recursion(arr[0])
+}
+console.log(main(transactions))
+// console.log(treeConverter(transactions))
