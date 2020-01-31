dad98fa7e8b1901b44222b648019320bbb95726b
Yurie <cutecupcakeandcute@gmail.com>
2020-01-31 12:25:04 +0900

    add amount property to the left child nodes

--- Basset_Practice_BinaryTree/tree.js
+++ Basset_Practice_BinaryTree/tree.js
@@ -8,11 +8,13 @@ const transactions = [
 // final data, ready for d3 tree()
 let tree = {
     name: "",
-    children: []
+    children: [],
+    amounts: []
 }
 
 let root = createRoot(transactions[0])
 createLeftChild(root.children[0])
+//createRightChild(root.children[0])
 
 function createRoot (obj) {
     tree.name = obj.source
@@ -21,10 +23,44 @@ function createRoot (obj) {
         child["name"] = obj.targets[i]
         tree.children.push(child)
     }
+    for (let j = 0; j < obj.amounts.length; j++) {
+        let amount = {}
+        amount["amount"] = obj.amounts[j]
+        tree.amounts.push(amount)
+    }
     return tree
 }
 
 function createLeftChild (childObj) {
+    childObj.children = []
+    childObj.amounts = []
+    // looking for children && amounts
+    for (let k = 0; k < transactions.length; k++) {
+        if (transactions[k].source === childObj.name) {
+            // children
+            for (let l = 0; l < transactions[k].targets.length; l++) {
+                let child = {}
+                child["name"] = transactions[k].targets[l]
+                childObj.children.push(child)
+            }
+            // amounts
+            for (let m = 0; m < transactions[k].amounts.length; m++) {
+                let amount = {}
+                amount["amount"] = transactions[k].amounts[m]
+                childObj.amounts.push(amount)
+            }
+        }
+    }
+
+    if (childObj.children[0] === undefined) {
+        return
+    }
+
+    createLeftChild(childObj.children[0])
+    console.log(JSON.stringify(root))
+}
+
+function createRightChild (childObj) {
     childObj.children = []
     for (let k = 0; k < transactions.length; k++) {
         if (transactions[k].source === childObj.name) {
@@ -36,11 +72,11 @@ function createLeftChild (childObj) {
         }
     }
 
-    if (childObj.children[0] === undefined) {
+    if (childObj.children[1] === undefined) {
         return
     }
 
-    createLeftChild(childObj.children[0])
+    createLeftChild(childObj.children[1])
     console.log(JSON.stringify(root))
 }
 
