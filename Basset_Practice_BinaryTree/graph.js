483565b1a04cb1b8dc9dd7b7c8ed3611c154cc58
Yurie <cutecupcakeandcute@gmail.com>
2020-02-04 15:39:40 +0900

    group update, enter, exit events inside one function called update()

--- Basset_Practice_BinaryTree/graph.js
+++ Basset_Practice_BinaryTree/graph.js
@@ -78,6 +78,21 @@ const data = {
  const margin = { top: 30, right: 30, bottom: 30, left: 40},
  width = 500 - margin.left - margin.right,
  height = 500 - margin.top - margin.bottom
+
+
+ // set children to null, to only display the root node
+   // root 
+   const root = d3.hierarchy(data)
+   root.x0 = root.x
+   root.y0 = root.y
+ 
+
+// children
+   root.descendants().forEach((d, i) => {
+      d.id = i
+      d._children = d.children
+      if (d.depth !== 4) d.children = null
+   })
  
  // canvas
  let canvas = d3.select("body")
@@ -87,15 +102,8 @@ const data = {
                 .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-let tree_d3 = d3.tree().size([height, width])
-const root = d3.hierarchy(data)
-tree_d3(root)
-
-const nodes = root.descendants().reverse()
-const links = root.links()
-
-const node = canvas.selectAll(".node")
-    .data(nodes, d => d.id)
+const gNode = canvas.selectAll(".node")
+    .data(nodes)
     .enter()
     .append("g")
         .attr("class", "node")
@@ -103,19 +111,19 @@ const node = canvas.selectAll(".node")
         .attr("cursor", "pointer")
         .attr("pointer-events", "all")
 
-    node.append("circle")
+    gNode.append("circle")
         .attr("r", 10)
         .attr("fill", "#ADAD")
 
-    node.append("text")
+    gNode.append("text")
         .text( d => d.data.name )
 
 let line = d3.line()
                 .x( d => d.x )
                 .y( d => d.y )
 
-const link = canvas.selectAll(".link")
-    .data(links, d => d.target.id)
+const gLink = canvas.selectAll(".link")
+    .data(links)
     .enter()
     .append("g")
         .attr("class", "link")
@@ -159,55 +167,56 @@ const arrowheads = d3.select("svg")
                     .attr('fill', '#ADADAD')
                     .style('stroke', 'none')
 
+                    
+function update(source) {
+   // durations for animation
+   const duration = d3.event ? 250 : 0;
+   const nodes = root.descendants().reverse()
+   const links = root.links()
 
-// set children to null, to only display the root node
-   // root 
-      root.x0 = root.x
-      root.y0 = root.y
+   // Compute the new tree layout
+   let tree_d3 = d3.tree().size([height, width])
+   tree_d3(root)
+
+   //transitions
+   const transitions = canvas.transition()
+                     .duration(duration)
+
+   // update, enter, exit for nodes
+   const node = gNode.selectAll("g")
+               .data(nodes, d => d.id)
+
+   const nodeEnter = node.enter().append("g")
+                     .attr("transform", d => `translate(${source.x0}, ${source.y0})`)
+                     .attr("fill-opacity", 1)
+                     .attr("stroke-opacity", 1)
+                     .on("click", d => {
+                        d.children = d.children ? null : d._children
+                        update(d)
+                     })
+
+   nodeEnter.append("circle")
+            .attr("r", 10)
+            .attr("fill", d => d._children ? "#ADAD" : "#524400")
    
-   // children
-      root.descendants().forEach((d, i) => {
-         d.id = i;
-         d._children = d.children;
-         if (d.depth !== 4) d.children = null;
-      })
+   nodeEnter.append("text")
+            .text( d => d.data.name )
+               .clone(true).lower()
 
-// durations for animation
-      const duration = d3.event ? 250 : 0;
+   const nodeUpdate = node.merge(nodeEnter).transition(transitions)
+                  .attr("transform", d => `translate(${d.x}, ${d.y})`)
+                  .attr("fill-opacity", 1)
+                  .attr("fill-opacity", 1)
 
-//transitions
-      const transitions = canvas.transition()
-                           .duration(duration)
-
-// enter, exit for nodes
-      const nodeEnter = node.enter().append("g")
-                        .attr("transform", d => `translate(${source.x0}, ${source.y0})`)
-                        .attr("fill-opacity", 1)
-                        .attr("stroke-opacity", 1)
-                        .on("click", d => {
-                           d.children = d.children ? null : d._children
-                           update(d)
-                        })
-
-      nodeEnter.append("circle")
-               .attr("r", 10)
-               .attr("fill", d => d._children ? "#ADAD" : "#524400")
-      
-      nodeEnter.append("text")
-               .text( d => d.data.name )
-                  .clone(true).lower()
-      
-      const nodeUpdate = node.merge(nodeEnter).transition(transitions)
-                        .attr("transform", d => `translate(${d.x}, ${d.y})`)
-                        .attr("fill-opacity", 1)
-                        .attr("fill-opacity", 1)
-
-      const nodeExit = node.exit().transition(transitions).remove()
-                        .attr("transform", d => `translate(${source.x}, ${source.y})`)
-                        .attr("fill-opacity", 0)
-                        .attr("fill-opacity", 0)
-                        
-// enter, exit for links
+   const nodeExit = node.exit().transition(transitions).remove()
+                  .attr("transform", d => `translate(${source.x}, ${source.y})`)
+                  .attr("fill-opacity", 0)
+                  .attr("fill-opacity", 0)
+   
+    // update, enter, exit for links
+   const link = gLink.selectAll("path")
+               .data(links, d => d.target.id)
+   
    const linkEnter = link.enter().append("path")
                         .attr("d", d => {
                            return line([d.source, d.target])
@@ -222,3 +231,16 @@ const arrowheads = d3.select("svg")
       .attr("d", d => {
          return line([d.source, d.target])
       })
+}
+
+update(root);
+
+
+
+
+
+
+      
+
+                        
+
