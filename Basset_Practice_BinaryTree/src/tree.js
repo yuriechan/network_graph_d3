15a8e8ba8b66346a3cb3ae04f606d5844f9c71ba
Yurie <cutecupcakeandcute@gmail.com>
2020-02-08 01:01:01 +0900

    install webpack && add test code to use import/export syntax

---
+++ Basset_Practice_BinaryTree/src/tree.js
@@ -0,0 +1,107 @@
+const bro = (greeting) => {
+    return `${greeting}, bro`
+}
+
+export { bro }
+
+// original data
+const transactions = [
+    { source: 0, targets: [1, 2], amounts: [1.5, 0.5] },
+    { source: 1, targets: [3, 4], amounts: [1.2, 0.3] },
+    { source: 3, targets: [5, 6], amounts: [0.8, 0.2] }
+]
+
+// final data, ready for d3 tree()
+let tree = {
+    name: "",
+    children: [],
+    amounts: []
+}
+
+const binaryTree = createTree()
+
+function createTree () {
+    let root = createRoot(transactions[0])
+    createLeftChild(root.children[0])
+    createRightChild(root.children[1])
+    console.log(JSON.stringify(root))
+    return root
+}
+
+// helper function 1 
+function createRoot (obj) {
+    tree.name = obj.source
+    for (let i = 0; i < obj.targets.length; i++) {
+        let child = {}
+        child["name"] = obj.targets[i]
+        tree.children.push(child)
+    }
+    for (let j = 0; j < obj.amounts.length; j++) {
+        let amount = {}
+        amount["amount"] = obj.amounts[j]
+        tree.amounts.push(amount)
+    }
+    return tree
+}
+
+// helper function 2 
+function createLeftChild (childObj) {
+    childObj.children = []
+    childObj.amounts = []
+    // looking for children && amounts
+    for (let k = 0; k < transactions.length; k++) {
+        if (transactions[k].source === childObj.name) {
+            // children
+            for (let l = 0; l < transactions[k].targets.length; l++) {
+                let child = {}
+                child["name"] = transactions[k].targets[l]
+                childObj.children.push(child)
+            }
+            // amounts
+            for (let m = 0; m < transactions[k].amounts.length; m++) {
+                let amount = {}
+                amount["amount"] = transactions[k].amounts[m]
+                childObj.amounts.push(amount)
+            }
+        }
+    }
+
+    if (childObj.children[0] === undefined) {
+        return
+    } else {
+        createLeftChild(childObj.children[0])
+        createRightChild(childObj.children[1])
+    }
+}
+
+function createRightChild (childObj) {
+
+    childObj.children = []
+    childObj.amounts = []
+    // looking for children && amounts
+    for (let k = 0; k < transactions.length; k++) {
+        if (transactions[k].source === childObj.name) {
+            // children
+            for (let l = 0; l < transactions[k].targets.length; l++) {
+                let child = {}
+                child["name"] = transactions[k].targets[l]
+                childObj.children.push(child)
+            }
+            // amounts
+            for (let m = 0; m < transactions[k].amounts.length; m++) {
+                let amount = {}
+                amount["amount"] = transactions[k].amounts[m]
+                childObj.amounts.push(amount)
+            }
+        }
+    }
+
+    if (childObj.children[1] === undefined) {
+        return
+    } else {
+        createRightChild(childObj.children[1])
+        createLeftChild(childObj.children[0])
+    }
+}
+
+
