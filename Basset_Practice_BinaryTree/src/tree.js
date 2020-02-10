5155ea8f7e4c18b60caa56d104b0ec37666f6307
Yurie <cutecupcakeandcute@gmail.com>
2020-02-10 16:36:41 +0900

    remove old function

--- Basset_Practice_BinaryTree/src/tree.js
+++ Basset_Practice_BinaryTree/src/tree.js
@@ -7,7 +7,7 @@ const transactions = [
 
 let makeTree = (transactions, parent) => {
    let node = {}
-   node.source = parent
+   node.name = parent
    node.children = []
    node.amounts = []
    transactions.filter(trsc => {return trsc.source === parent})
@@ -21,97 +21,7 @@ let makeTree = (transactions, parent) => {
                 })
    return node
 }
-console.log(JSON.stringify(makeTree(transactions, 0), null, 2))
-// final data, ready for d3 tree()
-let tree = {
-    name: "",
-    children: [],
-    amounts: []
-}
-
-// function createTree () {
-//     let root = createRoot(transactions[0])
-//     createLeftChild(root.children[0])
-//     createRightChild(root.children[1])
-//     console.log(JSON.stringify(root))
-//     return root
-// }
-
-// helper function 1 
-function createRoot (obj) {
-    tree.name = obj.source
-    for (let i = 0; i < obj.targets.length; i++) {
-        let child = {}
-        child["name"] = obj.targets[i]
-        tree.children.push(child)
-    }
-    for (let j = 0; j < obj.amounts.length; j++) {
-        let amount = {}
-        amount["amount"] = obj.amounts[j]
-        tree.amounts.push(amount)
-    }
-    return tree
-}
-
-// helper function 2 
-function createLeftChild (childObj) {
-    childObj.children = []
-    childObj.amounts = []
-    // looking for children && amounts
-    for (let k = 0; k < transactions.length; k++) {
-        if (transactions[k].source === childObj.name) {
-            // children
-            for (let l = 0; l < transactions[k].targets.length; l++) {
-                let child = {}
-                child["name"] = transactions[k].targets[l]
-                childObj.children.push(child)
-            }
-            // amounts
-            for (let m = 0; m < transactions[k].amounts.length; m++) {
-                let amount = {}
-                amount["amount"] = transactions[k].amounts[m]
-                childObj.amounts.push(amount)
-            }
-        }
-    }
-
-    if (childObj.children[0] === undefined) {
-        return
-    } else {
-        createLeftChild(childObj.children[0])
-        createRightChild(childObj.children[1])
-    }
-}
-
-function createRightChild (childObj) {
-
-    childObj.children = []
-    childObj.amounts = []
-    // looking for children && amounts
-    for (let k = 0; k < transactions.length; k++) {
-        if (transactions[k].source === childObj.name) {
-            // children
-            for (let l = 0; l < transactions[k].targets.length; l++) {
-                let child = {}
-                child["name"] = transactions[k].targets[l]
-                childObj.children.push(child)
-            }
-            // amounts
-            for (let m = 0; m < transactions[k].amounts.length; m++) {
-                let amount = {}
-                amount["amount"] = transactions[k].amounts[m]
-                childObj.amounts.push(amount)
-            }
-        }
-    }
-
-    if (childObj.children[1] === undefined) {
-        return
-    } else {
-        createRightChild(childObj.children[1])
-        createLeftChild(childObj.children[0])
-    }
-}
-
-//module.export  = { transactions, createTree, createRoot, createLeftChild, createRightChild }
+//console.log(JSON.stringify(makeTree(transactions, 0), null, 2))
+const treeData = makeTree(transactions, 0)
+export { transactions, makeTree, treeData }
 
