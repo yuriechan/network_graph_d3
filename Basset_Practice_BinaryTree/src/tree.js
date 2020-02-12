d092d23068794f9ee6c380fb3aaf2f6b6148a8e6
Yurie <cutecupcakeandcute@gmail.com>
2020-02-12 16:26:53 +0900

    comment out test data

--- Basset_Practice_BinaryTree/src/tree.js
+++ Basset_Practice_BinaryTree/src/tree.js
@@ -1,9 +1,9 @@
-// original data
-const transactions = [
-    { source: 0, targets: [1, 2], amounts: [1.5, 0.5] },
-    { source: 1, targets: [3, 4], amounts: [1.2, 0.3] },
-    { source: 3, targets: [5, 6], amounts: [0.8, 0.2] }
-]
+// test data
+// const transactions = [
+//     { source: 0, targets: [1, 2], amounts: [1.5, 0.5] },
+//     { source: 1, targets: [3, 4], amounts: [1.2, 0.3] },
+//     { source: 3, targets: [5, 6], amounts: [0.8, 0.2] }
+// ]
 
 let makeTree = (transactions, parent) => {
    let node = {}
@@ -22,6 +22,6 @@ let makeTree = (transactions, parent) => {
    return node
 }
 //console.log(JSON.stringify(makeTree(transactions, 0), null, 2))
-const treeData = makeTree(transactions, 0)
-export { transactions, makeTree, treeData }
+//const treeData = makeTree(transactions, 0)
+//export { makeTree, treeData }
 
