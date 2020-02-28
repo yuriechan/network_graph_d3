529406c4ff5341b624b813873bbbdd4dfdf009d4
Yurie <cutecupcakeandcute@gmail.com>
2020-02-28 17:10:57 +0900

    add gitignore file && import library in custom component

--- vue-d3/src/components/D3TreeGraph.vue
+++ vue-d3/src/components/D3TreeGraph.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        
-    </div>
+    <svg>
+
+    </svg>
 </template>
 
 <script>
+import * as d3 from 'd3'
+
 export default {
-    name: 'D3TreeGraph'
+    name: 'D3TreeGraph',
+    data: function () {
+        return {
+            testTransactionData: {
+               
+            }
+        }
+    }
 }
 </script>
 
