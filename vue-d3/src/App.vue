2e8c7f47b69b6d56f66831271b20388fbd9b348f
Yurie <cutecupcakeandcute@gmail.com>
2020-02-28 15:25:52 +0900

    import custom component into app component

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <D3TreeGraph />
   </div>
 </template>
 
 <script>
+import D3TreeGraph from './components/D3TreeGraph'
 
 export default {
   name: 'App',
   components: {
+    D3TreeGraph
   }
 }
 </script>
---
+++ vue-d3/src/components/D3TreeGraph.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'D3TreeGraph'
+}
+</script>
+
+<style scoped>
+</style>
